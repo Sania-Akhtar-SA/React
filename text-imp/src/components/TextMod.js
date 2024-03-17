a7758c854108969fdd735486855bc0695b8da77e
Sania Akhtar <151676359+Sania-Akhtar-SA@users.noreply.github.com>
2024-03-18 00:10:17 +0530

    Update TextMod.js

--- text-imp/src/components/TextMod.js
+++ text-imp/src/components/TextMod.js
@@ -12,35 +12,65 @@ export default function TextMod(props) {
 
   const handleUpperCase = () =>{
     setText(text.toUpperCase());
-    props.alert("Converted to UpperCase !", "success");
+    if(text===""){
+      props.alert("Enter text to Convert into UpperCase !", "warning");
+    }
+    else{
+      props.alert("Converted to UpperCase !", "success");
+    }
   }
 
   const handleLowerCase = () =>{
     setText(text.toLowerCase())
-    props.alert("Converted to LowerCase !", "success");
+    if(text===""){
+      props.alert("Enter text to Convert into LowerCase !", "warning");
+    }
+    else{
+      props.alert("Converted to LowerCase !", "success");
+    }
   }
 
   const handleCopy = () =>{
     var text = document.getElementById("myText");
     text.select();
     navigator.clipboard.writeText(text.value);
-    props.alert("Text Copied to Clipboard !", "success");
+    if(text===""){
+      props.alert("Enter text to Copy !", "warning");
+    }
+    else{
+      props.alert("Text Copied !", "success");
+    }
   }
 
   const handleSpace = () =>{
     var newText=text.split(/[ ]+/);
     setText(newText.join(" "));
-    props.alert("Extra space has been removed !", "success");
+    if(text===""){
+      props.alert("Enter text to remove extra space !", "warning");
+    }
+    else{
+      props.alert("Extra space removed !", "success");
+    }
   }
 
   const handleClear = () =>{
     setText("")
-    props.alert("Text Cleared !", "success");
+    if(text===""){
+      props.alert("Already clear !", "warning");
+    }
+    else{
+      props.alert("Text Cleared !", "success");
+    }
   }
 
   const handleRepeat = () =>{
     setText(text + text.substring(0,text.length))
-    props.alert("Repeated Text !","success");
+    if(text===""){
+      props.alert("Enter text to repeat !", "warning");
+    }
+    else{
+      props.alert("Text Repeated !", "success");
+    }
   }
   
   return (
@@ -59,7 +89,7 @@ export default function TextMod(props) {
     </div>
     <div className={`container my-auto mt-3 text-${props.Mode==='light'?'dark':'light'} bg-${props.Mode}`}>
       <h2 className='my-5'><u>Your Text Summary</u></h2>
-      <p className='my-3'><b>{text===""?0:text.split(" ").length}</b> words, <b>{text.length}</b> characters and <b>{text!=="."?0:text.split(".").length}</b> sentences.</p>
+      <p className='my-3'><b>{text===""?0:text.split(" ").length}</b> words, <b>{text.length}</b> characters and <b>{(text.split(".").length)-1}</b> sentences.</p>
       <p className='my-3'><b>{text===""?0:text.split(" ").length * 0.008}</b> mins to read the text</p>
       <h4 className='my-5'><u>Preview</u></h4>
       <p className="my-4">{text}</p>
