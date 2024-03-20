6b712a8fb254efc3d0aea6a170867e088c5157aa
Sania Akhtar <151676359+Sania-Akhtar-SA@users.noreply.github.com>
2024-03-20 20:31:06 +0530

    Update TextMod.js

--- text-imp/src/components/TextMod.js
+++ text-imp/src/components/TextMod.js
@@ -34,7 +34,7 @@ export default function TextMod(props) {
     var text = document.getElementById("myText");
     text.select();
     navigator.clipboard.writeText(text.value);
-    if(text===""){
+    if(text.length === 0){
       props.alert("Enter text to Copy !", "warning");
     }
     else{
@@ -64,7 +64,7 @@ export default function TextMod(props) {
   }
 
   const handleRepeat = () =>{
-    setText(text + text.substring(0,text.length))
+    setText(text +" " + text.substring(0,text.length))
     if(text===""){
       props.alert("Enter text to repeat !", "warning");
     }
@@ -72,27 +72,27 @@ export default function TextMod(props) {
       props.alert("Text Repeated !", "success");
     }
   }
-  
+  const p="Enter text here ...!!"
   return (
     <div className={`bg-${props.Mode} `}>
     <div className={`container my-4 py-2 px-4 bg-${props.Mode} `}>
         <h3><label htmlFor="myText" className={`form-label text-${props.Mode==='light'?'dark':'light'} bg-${props.Mode}`} >{props.heading}</label></h3>
-        <textarea className={`form-control my-3 mb-7 text-${props.Mode==='light'?'dark':'light'} bg-${props.Mode}`} placeholder="Enter text here ..." value ={text} onChange={handleOnChange} id="myText" rows="8" ></textarea>
+        <textarea className={`form-control my-3 mb-7 text-${props.Mode==='light'?'dark':'light'} bg-${props.Mode}`} placeholder={p} value ={text} onChange={handleOnChange} id="myText" rows="8" ></textarea>
     </div>
     <div className='container my-5'>
-    <button className={`btn mx-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleUpperCase}>Covert to UpperCase</button>
-    <button className={`btn mx-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleLowerCase} >Covert to LowerCase</button>
-    <button className={`btn mx-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleCopy} >Copy To Clipboard</button>
-    <button className={`btn mx-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleSpace} >Remove Extra Space</button>
-    <button className={`btn mx-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleClear} >Clear Text</button>
-    <button className={`btn mx-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleRepeat} >Repeat Text</button>
+    <button className={`btn mx-2 my-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleUpperCase}>Covert to UpperCase</button>
+    <button className={`btn mx-2 my-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleLowerCase} >Covert to LowerCase</button>
+    <button className={`btn mx-2 my-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleSpace} >Remove Extra Space</button>
+    <button className={`btn mx-2 my-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleClear} >Clear Text</button>
+    <button className={`btn mx-2 my-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleRepeat} >Repeat Text</button>
+    <button disabled={text===""} className={`btn mx-2 my-2 btn-outline-${props.Mode==='light'?'dark':'light'} btn-${props.Mode}`} onClick={handleCopy} >Copy To Clipboard</button>
     </div>
     <div className={`container my-auto mt-3 text-${props.Mode==='light'?'dark':'light'} bg-${props.Mode}`}>
       <h2 className='my-5'><u>Your Text Summary</u></h2>
-      <p className='my-3'><b>{text===""?0:text.split(" ").length}</b> words, <b>{text.length}</b> characters and <b>{(text.split(".").length)-1}</b> sentences.</p>
-      <p className='my-3'><b>{text===""?0:text.split(" ").length * 0.008}</b> mins to read the text</p>
+      <p className='my-3'><b>{text==="" || text===" " ?0:text.split(/\s+[a-zA-Z]/).length}</b> words, <b>{text.length}</b> characters and <b>{(text.split(".").length)-1}</b> sentences.</p>
+      <p className='my-3'><b>{text===""?0:text.split(/\s+[a-zA-Z]/).length * 0.008}</b> mins to read the text</p>
       <h4 className='my-5'><u>Preview</u></h4>
-      <p className="my-4">{text}</p>
+      <div className="my-4" style={{height:"150px"}}>{text.length>0?text:"Nothing to preview."}</div>
     </div>
     </div>
   )
